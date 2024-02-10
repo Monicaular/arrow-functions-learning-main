7a8d41d1313af18fdfad20d7e9f8c107e844baca
Monicaular <moniq.iancu@gmail.com>
2024-02-10 14:23:16 +0000

    arrow functions

---
+++ arrow-functions.js
@@ -0,0 +1,42 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node arrow-functions.js in the terminal
+ */
+
+// Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     //code block
+//     return a+b;
+// }
+// let sum = addTwoNumbers(3,5)
+// console.log(sum);
+
+// Arrow Function With Parameters
+// const addTwoNumbers = (a,b) => {
+//     return a+b;
+// }
+
+// let sum = addTwoNumbers(3,5)
+// console.log(sum);
+
+
+// Single Line Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => a + b;
+let sum = addTwoNumbers(6,4);
+console.log(sum);
+
+// Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello World');
+
+const sayHello = () => console.log('hello');
+sayHello();
+
+// Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
